f82f7f7a5e01ebf7d7e8aaf1beb717fc93dff207
Katt Weller <kattweller3@gmail.com>
2020-10-02 21:40:40 -0400

    pushed java_events folder

---
+++ java_events/scorekeeper.js
@@ -0,0 +1,12 @@
+var p1_button = document.querySelector("#p1_button");
+var p2_button = document.querySelector("#p2_button");
+
+var p1_score, p2_score = 0;
+
+p1_button.addEventListener("click", function(){
+	console.log("player 1 scored")
+})
+
+p2_button.addEventListener("click", function(){
+	console.log("player 2 scored")
+})
